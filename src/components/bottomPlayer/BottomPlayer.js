6a603224d82a34bd7eef5a0c795f9d8c1e0a02f8
james-muriithi <muriithijames556@gmail.com>
2020-07-16 16:04:01 +0300

    added bottom drawer

--- src/components/bottomPlayer/BottomPlayer.js
+++ src/components/bottomPlayer/BottomPlayer.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useState } from "react"
 
 import { makeStyles, Typography } from "@material-ui/core"
 import LinearProgress from "@material-ui/core/LinearProgress"
@@ -8,6 +8,10 @@ import MusicNote from "@material-ui/icons/MusicNote"
 import PlayIcon from "@material-ui/icons/PlayArrowSharp"
 import Avatar from "@material-ui/core/Avatar"
 import IconButton from "@material-ui/core/IconButton"
+import SwipeableDrawer from '@material-ui/core/SwipeableDrawer';
+
+import SongHeader from '../songHeader/SongHeader';
+
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -30,25 +34,58 @@ const useStyles = makeStyles(theme => ({
 }))
 
 function BottomPlayer() {
+  const [ open, setOpen ] = useState(false)
   const classes = useStyles()
+
+  const toggleDrawer = event => {
+    if (
+      event &&
+      event.type === "keydown" &&
+      (event.key === "Tab" || event.key === "Shift")
+    ) {
+      return
+    }
+
+    setOpen(prevState => !prevState)
+  }
   return (
-    <Paper className={classes.root}>
-      <LinearProgress variant="determinate" value={12} />
-      <div className="now-playing-container">
-        <Avatar variant="square" className={classes.square}>
-          <MusicNote style={{ fontSize: "35px" }} />
-        </Avatar>
-        <div className="song-name">
-          <Typography variant="body1">hello there</Typography>
-          <Typography variant="caption" style={{ color: "#b2b2b2" }}>
-            Unknown artist
+    <>
+      <Paper className={classes.root} onClick={()=>{
+        setOpen(true)
+      }} >
+        <LinearProgress variant="determinate" value={12} />
+        <div className="now-playing-container">
+          <Avatar variant="square" className={classes.square}>
+            <MusicNote style={{ fontSize: "35px" }} />
+          </Avatar>
+          <div className="song-name">
+            <Typography variant="body1">hello there</Typography>
+            <Typography variant="caption" style={{ color: "#b2b2b2" }}>
+              Unknown artist
           </Typography>
+          </div>
+          <IconButton className={classes.icon}>
+            <PlayIcon className={classes.icon} />
+          </IconButton>
         </div>
-        <IconButton className={classes.icon}>
-          <PlayIcon className={classes.icon} />
-        </IconButton>
-      </div>
-    </Paper>
+      </Paper>
+      <SwipeableDrawer
+        anchor='bottom'
+        open={open}
+        onClose={toggleDrawer}
+        onOpen={toggleDrawer}
+        ModalProps={{
+          style: {zIndex: 10000}
+        }}
+        PaperProps={{
+          style:{ height: '100%'}
+        }}
+      >
+        <div>
+          <SongHeader toggleDrawer={toggleDrawer} />
+        </div>
+      </SwipeableDrawer>
+    </>
   )
 }
 
