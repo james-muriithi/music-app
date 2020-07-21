4a5386587a64dd5be960cec5370e31c3256f26f3
james-muriithi <muriithijames556@gmail.com>
2020-07-21 14:23:43 +0300

    added appropriate icon sizes

--- src/utils/media-session.js
+++ src/utils/media-session.js
@@ -18,22 +18,27 @@ const addNewSong = id => {
     album: "Unknown Albumn",
     artwork: [
       {
-        src: require("../images/favicon.png"),
+        src: require("../images/icons/icon-96x96.png"),
         sizes: "96x96",
         type: "image/png",
       },
       {
-        src: require("../images/favicon.png"),
+        src: require("../images/icons/icon-144x144.png"),
         sizes: "144x144",
         type: "image/png",
       },
       {
-        src: require("../images/favicon.png"),
+        src: require("../images/icons/icon-192x192.png"),
         sizes: "192x192",
         type: "image/png",
       },
       {
-        src: require("../images/favicon.png"),
+        src: require("../images/icons/icon-256x256.png"),
+        sizes: "256x256",
+        type: "image/png",
+      },
+      {
+        src: require("../images/icons/icon-512x512.png"),
         sizes: "512x512",
         type: "image/png",
       },
