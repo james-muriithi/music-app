ee49b115f00465d11e6b8459d0773932b2815704
james-muriithi <muriithijames556@gmail.com>
2020-07-25 23:49:07 +0300

    search input

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react"
 
-import { makeStyles } from "@material-ui/core/styles"
+import { makeStyles, fade } from "@material-ui/core/styles"
 import AppBar from "@material-ui/core/AppBar"
 import Toolbar from "@material-ui/core/Toolbar"
 import Typography from "@material-ui/core/Typography"
@@ -16,7 +16,8 @@ import PlayListIcon from "@material-ui/icons/List"
 import HomeIcon from "@material-ui/icons/Home"
 import SettingsIcon from "@material-ui/icons/Settings"
 import MenuIcon from "@material-ui/icons/Menu"
-import SearchIcon from "@material-ui/icons/Search"
+import InputBase from '@material-ui/core/InputBase';
+import CloseIcon from '@material-ui/icons/Close';
 
 import Logo from "../logo"
 
@@ -34,6 +35,50 @@ const useStyles = makeStyles(theme => ({
     width: 250,
     [theme.breakpoints.down("sm")]: {
       width: 230,
+    },
+  },
+  search: {
+    position: 'relative',
+    borderRadius: theme.shape.borderRadius,
+    backgroundColor: fade(theme.palette.common.white, 0.15),
+    '&:hover': {
+      backgroundColor: fade(theme.palette.common.white, 0.25),
+    },
+    marginLeft: 0,
+    width: '100%',
+    [ theme.breakpoints.up('sm') ]: {
+      marginLeft: theme.spacing(1),
+      width: 'auto',
+    },
+    [ theme.breakpoints.down('sm') ]: {
+      height: '100%',
+    },
+  },
+  searchIcon: {
+    height: '100%',
+    position: 'absolute',
+    zIndex: 10,
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    '& button':{
+      color: '#fff'
+    }
+  },
+  inputRoot: {
+    color: 'inherit',
+  },
+  inputInput: {
+    padding: theme.spacing(1, 1, 1, 0),
+    // vertical padding + font size from searchIcon
+    paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
+    transition: theme.transitions.create('width'),
+    width: '100%',
+    [ theme.breakpoints.up('sm') ]: {
+      width: '12ch',
+      '&:focus': {
+        width: '20ch',
+      },
     },
   },
 }))
@@ -94,9 +139,21 @@ export default function Header() {
             Music App
           </Typography>
 
-          <IconButton aria-label="search" color="inherit">
-            <SearchIcon style={{ fontSize: "27px" }} />
-          </IconButton>
+          <div className={classes.search}>
+            <div className={classes.searchIcon}>
+              <IconButton>
+                <CloseIcon />
+              </IconButton>
+            </div>
+            <InputBase
+              placeholder="Search…"
+              classes={{
+                root: classes.inputRoot,
+                input: classes.inputInput,
+              }}
+              inputProps={{ 'aria-label': 'search' }}
+            />
+          </div>
         </Toolbar>
       </AppBar>
       <SwipeableDrawer
